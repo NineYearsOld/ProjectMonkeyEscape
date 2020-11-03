68848dd77c2e05665488fadc90cec4dc0ecf3a01
Raymond Tréville <raymond.treville@student.hogent.be>
2020-11-03 23:00:38 +0100

    Add project files.

---
+++ ProjectMonkeyEscape/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Drawing;
+namespace ProjectMonkey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateGame create = new CreateGame();
+        }
+    }
+}
